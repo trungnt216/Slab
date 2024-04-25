f7beaa698bc817f04c09e67c164904250af1d1e7
Nguyen Tuan Phong <85359726+NguyenTuanPhong2002@users.noreply.github.com>
2024-04-26 01:12:55 +0700

    Revert "create forgot password method"
    This reverts commit 0cb8bdf2d968f4b41c4a63ce129e8d80ea79f08b.

--- SaRLAB/SaRLAB.Application/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SaRLAB.Application.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
