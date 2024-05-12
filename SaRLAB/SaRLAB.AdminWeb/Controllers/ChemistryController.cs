1628402a3fb474128d016d708adf76b1e3946f95
Nguyen Tuan Phong <85359726+NguyenTuanPhong2002@users.noreply.github.com>
2024-05-12 21:43:22 +0700

    add the chemis controller

--- SaRLAB/SaRLAB.AdminWeb/Controllers/ChemistryController.cs
+++ SaRLAB/SaRLAB.AdminWeb/Controllers/ChemistryController.cs
@@ -1,8 +1,52 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SaRLAB.Models.Dto;
+using SaRLAB.Models.Entity;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net.Http.Headers;
+using System.Security.Claims;
 
 namespace SaRLAB.AdminWeb.Controllers
 {
     public class ChemistryController : Controller
     {
+        Uri baseAddress = new Uri("http://localhost:5200/api/");
+        private readonly HttpClient _httpClient;
+
+        private readonly IConfiguration _configuration;
+
+        UserDto userLogin = new UserDto();
+
+        public ChemistryController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = baseAddress;
+            _configuration = configuration;
+
+            string jwtToken = _configuration["JwtToken:Value"];
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.ReadJwtToken(jwtToken);
+
+            foreach (Claim claim in token.Claims)
+            {
+                if (claim.Type == ClaimTypes.Name)
+                {
+                    userLogin.Email = claim.Value;
+                }
+                else if (claim.Type == ClaimTypes.Role)
+                {
+                    userLogin.RoleName = claim.Value;
+                }
+            }
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", jwtToken);
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return View();
+        }
     }
 }
