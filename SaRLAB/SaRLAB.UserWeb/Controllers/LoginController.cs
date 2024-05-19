647d6f8fe57a3e049175e1d38af5c868c86a839c
Nguyen Tuan Phong <85359726+NguyenTuanPhong2002@users.noreply.github.com>
2024-05-19 10:51:37 +0700

    add view

--- SaRLAB/SaRLAB.UserWeb/Controllers/LoginController.cs
+++ SaRLAB/SaRLAB.UserWeb/Controllers/LoginController.cs
@@ -105,7 +105,7 @@ namespace SaRLAB.UserWeb.Controllers
 
                         if (claim.Value.Equals("Admin"))
                         {
-                            return View();
+                            return RedirectToAction("Index","HomePage");
                         }
                     }
                 }
@@ -114,7 +114,7 @@ namespace SaRLAB.UserWeb.Controllers
             else
             {
                 TempData["Error"] = "Không có tài khoản. Vui lòng thử lại!";
-                return View("Index");
+                return View();
             }
         }
 
