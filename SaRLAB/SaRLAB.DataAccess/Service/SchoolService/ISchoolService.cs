ab5d65ec74fde868fc2f5f50c1167580081d96c0
trungnt <nt.trungqb73@gmail.com>
2024-05-21 05:44:14 +0700

    school service

---
+++ SaRLAB/SaRLAB.DataAccess/Service/SchoolService/ISchoolService.cs
@@ -0,0 +1,17 @@
+﻿using SaRLAB.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaRLAB.DataAccess.Service.SchoolService
+{
+    public interface ISchoolService
+    {
+        List<School> GetAllSchool();
+        School GetSchoolById(int id);
+        int DeleteSchoolById(int id);
+        int UpdateSchoolById(int id, School school);
+    }
+}
