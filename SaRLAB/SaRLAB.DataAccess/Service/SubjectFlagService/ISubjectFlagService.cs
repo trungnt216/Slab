b0c0307014f346af4e00a1c1e77494e8c0d0d27a
trungnt <nt.trungqb73@gmail.com>
2024-06-08 00:04:08 +0700

    service

---
+++ SaRLAB/SaRLAB.DataAccess/Service/SubjectFlagService/ISubjectFlagService.cs
@@ -0,0 +1,17 @@
+﻿using SaRLAB.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaRLAB.DataAccess.Service.SubjectFlagService
+{
+    public  interface ISubjectFlagService
+    {
+
+        SubjectFlag getSubjectFlagByUserId(int userId);
+        int InsertSubjectFlag(SubjectFlag subjectFlag);
+        int updateSubjectFlag(int userId, SubjectFlag subjectFlag);
+    }
+}
