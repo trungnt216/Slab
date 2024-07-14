f84137dc778e5c0b95974aeaace6bb9a682fdf6d
trungnt <nt.trungqb73@gmail.com>
2024-07-14 22:48:31 +0700

    service

--- SaRLAB/SaRLAB.Models/Entity/School.cs
+++ SaRLAB/SaRLAB.Models/Entity/School.cs
@@ -2,9 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SaRLAB.Models.Entity
 {
@@ -13,10 +10,13 @@ namespace SaRLAB.Models.Entity
     {
         [Key]
         public int? ID { get; set; }
+
         [Required(ErrorMessage = "Vui lòng nhập tên trường!")]
         public string? Name { get; set; }
+
         [Required(ErrorMessage = "Vui lòng nhập địa chỉ!")]
         public string? Address { get; set; }
+
         public string? ChemLogo { get; set; }
         public string? BioLogo { get; set; }
         public string? PhysLogo { get; set; }
@@ -30,5 +30,32 @@ namespace SaRLAB.Models.Entity
         public string? BackupSubject5Logo { get; set; }
         public string? BackupSubject6Logo { get; set; }
 
+        // Bổ sung từ BackupSubject7Logo đến BackupSubject32Logo
+        public string? BackupSubject7Logo { get; set; }
+        public string? BackupSubject8Logo { get; set; }
+        public string? BackupSubject9Logo { get; set; }
+        public string? BackupSubject10Logo { get; set; }
+        public string? BackupSubject11Logo { get; set; }
+        public string? BackupSubject12Logo { get; set; }
+        public string? BackupSubject13Logo { get; set; }
+        public string? BackupSubject14Logo { get; set; }
+        public string? BackupSubject15Logo { get; set; }
+        public string? BackupSubject16Logo { get; set; }
+        public string? BackupSubject17Logo { get; set; }
+        public string? BackupSubject18Logo { get; set; }
+        public string? BackupSubject19Logo { get; set; }
+        public string? BackupSubject20Logo { get; set; }
+        public string? BackupSubject21Logo { get; set; }
+        public string? BackupSubject22Logo { get; set; }
+        public string? BackupSubject23Logo { get; set; }
+        public string? BackupSubject24Logo { get; set; }
+        public string? BackupSubject25Logo { get; set; }
+        public string? BackupSubject26Logo { get; set; }
+        public string? BackupSubject27Logo { get; set; }
+        public string? BackupSubject28Logo { get; set; }
+        public string? BackupSubject29Logo { get; set; }
+        public string? BackupSubject30Logo { get; set; }
+        public string? BackupSubject31Logo { get; set; }
+        public string? BackupSubject32Logo { get; set; }
     }
 }
