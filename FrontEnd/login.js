0ce2d1a16750c81751fa34466bcf2210c2884b9c
Vũ Trọng Hiếu <85973353+piuconal@users.noreply.github.com>
2024-08-17 20:29:55 +0700

    login

---
+++ FrontEnd/login.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const loginForm = document.getElementById("loginForm");
+  setTimeout(() => {
+    loginForm.classList.add("active");
+  }, 500);
+});
