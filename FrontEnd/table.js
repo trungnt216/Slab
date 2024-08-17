3b0240e3813cfc7d72d25df9b878bbf72a3d7c25
Vũ Trọng Hiếu <85973353+piuconal@users.noreply.github.com>
2024-08-17 18:48:41 +0700

    menu head

--- FrontEnd/table.js
+++ FrontEnd/table.js
@@ -1,9 +1,29 @@
 // header
 fetch("header.html")
   .then((response) => response.text())
-  .then(
-    (data) => (document.getElementById("header-placeholder").innerHTML = data)
-  );
+  .then((data) => {
+    document.getElementById("header-placeholder").innerHTML = data;
+
+    const menuBtn = document.getElementById("menu-btn");
+    if (menuBtn) {
+      const menuHead = document.querySelector(".menu_head");
+      const header = document.querySelector("header");
+      const olay = document.getElementById("olay");
+
+      menuBtn.addEventListener("click", () => {
+        menuHead.classList.toggle("active");
+        olay.classList.toggle("active");
+      });
+
+      olay.addEventListener("click", () => {
+        menuHead.classList.remove("active");
+        olay.classList.remove("active");
+      });
+    } else {
+      console.error("Phần tử menuBtn không tồn tại trong DOM.");
+    }
+  })
+  .catch((error) => console.error("Lỗi khi tải header:", error));
 
 //   page-data
 const rowsPerPage = 6;
@@ -13,4 +33,3 @@ const tableContainer = document.querySelector(".table-container");
 const prevButton = document.getElementById("prev");
 const nextButton = document.getElementById("next");
 const pageInfo = document.getElementById("page-info");
-
